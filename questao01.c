bba829eecadcc039ba0a621b6de7d2bf2804180f
Maria <madudu2007@gmail.com>
2026-08-22 20:15:17 -0300

    feat: finalizar questao 01

---
+++ questao01.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+int main() {
+    int a, b, soma;
+
+    printf("Primeiro valor: ");
+    scanf ("%d", &a);
+
+    printf("Segundo valor: ");
+    scanf ("%d", &b);
+
+    soma = a + b;
+    printf("Soma: %d\n", soma);
+    
+    return 0;
+
+}
