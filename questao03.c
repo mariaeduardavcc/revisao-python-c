18303e44a6dd6c2b59beba83d717adc2c3fed2e4
Maria <madudu2007@gmail.com>
2026-08-22 20:58:38 -0300

    adicione a 3

---
+++ questao03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    printf("Temperatura em Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = celsius * 9.0 / 5.0 + 32;
+
+    printf("Fahrenheit: %.2f\n", fahrenheit);
+
+    return 0;
+}
